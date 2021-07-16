afd81fcd769c63812f55c41fbf93f88663a7b089
NACIRI Hamza <naciri.nhamza@gmail.com>
2021-07-16 10:44:58 +0100

    add landing page template

---
+++ design/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans': ['Montserrat'],
+      'serif': ['Montserrat'],
+      'mono': ['Montserrat'],
+      'display': ['Montserrat'],
+      'body': ['Montserrat']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
